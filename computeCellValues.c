0effb75cca25180f69817fa6a773866930463306
Andre Malcher <andre.malcher@TUM>
2014-05-26 10:09:14 +0200

    Improved code of computeVelocity to make it suitable for vectorization.

--- computeCellValues.c
+++ computeCellValues.c
@@ -14,29 +14,32 @@ void computeDensity(const double *const currentCell, double *density){
 
 void computeVelocity(const double * const currentCell, const double * const density, double *velocity) {
     /* Initializing velocity vector to 0 */
-    velocity[0] = 0.0;
-    velocity[1] = 0.0;
-    velocity[2] = 0.0;
+	memset(velocity, 0, sizeof *velocity * D);
 
     /* Compute velocity by momentum equation */
-    for (int j = 0; j < D; ++j) {
-        for (int i = 0; i < Q; ++i) {
-           velocity[j] += currentCell[i] * LATTICEVELOCITIES[i][j];
+	for (int d = 0; d < D; ++d) {
+		for (int q = 0; q < Q; ++q) {
+           velocity[d] += currentCell[q] * LATTICEVELOCITIES[q][d];
         }
-        velocity[j] /= *density;
-    }    
+    }
+	for (int d = 0; d < D; ++d) {
+		velocity[d] /= *density;
+	}
 }
 
 void computeFeq(const double * const density, const double * const velocity, double *feq){
-	for (int i=0; i<Q; ++i) {
-        double c_dot_u = 0.0;
-        double u_dot_u = 0.0;
-		for (int j = 0; j < D; ++j) {
+	double c_dot_u;
+	double u_dot_u;
+
+	for (int q=0; q<Q; ++q) {
+        c_dot_u = 0.0;
+        u_dot_u = 0.0;
+		for (int d = 0; d < D; ++d) {
             /* Compute dot products needed for the computation of feq */
-            c_dot_u += LATTICEVELOCITIES[i][j] * velocity[j];
-            u_dot_u += velocity[j] * velocity[j];
+            u_dot_u += velocity[d] * velocity[d];
+            c_dot_u += LATTICEVELOCITIES[q][d] * velocity[d];
 		}
-		feq[i] = LATTICEWEIGHTS[i] * (*density) * (1 + (c_dot_u)/(C_S*C_S) +
+		feq[q] = LATTICEWEIGHTS[q] * (*density) * (1 + (c_dot_u)/(C_S*C_S) +
 			(c_dot_u)*(c_dot_u)/(2*C_S*C_S*C_S*C_S) - u_dot_u/(2*C_S*C_S));
 	}
 }
