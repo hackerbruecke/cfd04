7fce390a2098e795b5ac1709d83655f6cdf51306
Andre Malcher <andre.malcher@TUM>
2014-05-26 10:06:53 +0200

    Cleaned up code for boundary treatment.

--- boundary.c
+++ boundary.c
@@ -1,162 +1,71 @@
+#include <stdio.h>
 #include "boundary.h"
 #include "LBDefinitions.h"
 #include "computeCellValues.h"
 
-#if 1
-
+/* Check whether (x,y,z) is inside the domain bounds, including the ghost layer */
 static inline int inBounds(int x, int y, int z, int xlength) {
 	return  x > 0 && x < xlength+1 && y > 0 && y < xlength+1 && z > 0 && z < xlength+1;
 }
 
+/* Computes the no-slip condition for cell (x,y,z)
+ * if the respective neighbor determined by i is within bounds and a fluid cell */
+static inline void computeNoSlipCondition(int xlength, int x, int y, int z, int i, int dx, int dy, int dz,
+		double *collideField, const int * const flagField) {
+	/* Check if the i-th directions is facing the fluid particles and update accordingly */
+	if (inBounds(x+dx, y+dy, z+dz, xlength) && flagField[fidx(xlength, x+dx, z+dy, z+dz)] == FLUID) {
+		collideField[idx(xlength, x, y, z, i)] = collideField[idx(xlength, x + dx, y + dy, z + dz, inv(i))];
+	}
+}
+
+/* Computed the moving-wall condition for cell (x,y,z)
+ * if the respective neighbor determined by i is within bounds and a fluid cell */
+static inline void computeMovingWallCondition(int xlength, int x, int y, int z, int i, int dx, int dy, int dz,
+		double *collideField, const int * const flagField, const double * const wallVelocity) {
+	if (inBounds(x+dx, y+dy, z+dz, xlength) && flagField[fidx(xlength, x+dx, y+dy, z+dz)] == FLUID) {
+		double *currentCell = &collideField[idx(xlength, x+dx, y+dy, z+dz, 0)];
+
+		double density;
+		computeDensity(currentCell, &density);
+		double cu = 0; /*dot product of c_i and velocity of wall*/
+		for (int d = 0; d < D; ++d) {
+			cu += LATTICEVELOCITIES[i][d] * wallVelocity[d];
+		}
+		double finv = collideField[idx(xlength, x + dx, y + dy, z + dz, inv(i))];
+		collideField[idx(xlength, x, y, z, i)] = finv + 2*LATTICEWEIGHTS[i]*density*cu/(C_S*C_S);
+	}
+}
+
 void treatBoundary(double *collideField, int* flagField,
 		const double * const wallVelocity, int xlength) {
-	int i, dx, dy, dz;
-	double *currentCell;
-
-	double finv = 0;
-
 	/*Going through the whole boundary domain*/
 
 	/*For each boundary cell/particle we update all the distribution functions,
 	 that point towards fluid, according to boundary conditions*/
-
-	for (int x = 0; x < xlength; ++x) {
-		for (int y = 0; y < xlength; ++y) {
-			for (i = 0; i < Q; ++i) {
-				/*dx = c_i_x*dt, dt = 1*/
-				dx = LATTICEVELOCITIES[i][0];
-				dy = LATTICEVELOCITIES[i][1];
-				dz = LATTICEVELOCITIES[i][2];
-
-				/*Checking if the i-th directions is facing the fluid particles*/
-				/*y-z plane*/
-				if (inBounds(x+dx, y+dy, z+dz, xlength+1) && flagField[fidx(xlength, x + dx, y + dy, z+dz)] == FLUID) {
-						finv = collideField[idx(xlength, x + dx, y + dy, z + dz, Q - i - 1)];
-						collideField[idx(xlength, x, y, z, i)] = finv;
-					}
-				}
+	for (int i = 0; i < Q; ++i) {
+		/*dx = c_i_x*dt, dt = 1*/
+		int dx = LATTICEVELOCITIES[i][0];
+		int dy = LATTICEVELOCITIES[i][1];
+		int dz = LATTICEVELOCITIES[i][2];
+		for (int x = 0; x < xlength + 2; ++x) {
+			for (int y = 0; y < xlength + 2; ++y) {
+				computeNoSlipCondition(xlength, x, y, 0, i, dx, dy, dz, collideField, flagField);
+				computeMovingWallCondition(xlength, x, y, xlength+1, i, dx, dy, dz, collideField, flagField, wallVelocity);
 			}
 		}
-	}
 
-	for (int x = 0; x < xlength; ++x) {
-		for (int z = 0; z < xlength; ++z) {
-
+		for (int x = 0; x < xlength + 2; ++x) {
+			for (int z = 0; z < xlength + 2; ++z) {
+				computeNoSlipCondition(xlength, x, 0, z, i, dx, dy, dz, collideField, flagField);
+				computeNoSlipCondition(xlength, x, xlength+1, z, i, dx, dy, dz, collideField, flagField);
+			}
 		}
-	}
 
-	for (int x = 0; x < xlength; ++x) {
-		for (int z = 0; z < xlength; ++x) {
-
-		}
-	}
-
-	for (int z = 0; z < xlength[2] + 2; ++z) {
-		for (int y = 0; y < xlength[1] + 2; ++y) {
-			for (int x = 0; x < xlength[0] + 2; ++x) {
-
-				/* Conditions for NO_SLIP */
-				if (flagField[fidx(xlength, x, y, z)] == NO_SLIP) {
-					for (i = 0; i < Q; ++i) {
-
-						dx = LATTICEVELOCITIES[i][0];
-						dy = LATTICEVELOCITIES[i][1];
-						dz = LATTICEVELOCITIES[i][2];
-
-						if (x + dx > 0 && x + dx < xlength + 1 && y + dy > 0
-								&& y + dy < xlength + 1 && z + dz > 0
-								&& z + dz < xlength + 1) {
-
-							/*Checking if the i-th direction is facing the fluid particles*/
-
-							if (flagField[fidx(xlength, x + dx, y + dy, z + dz)]
-									== FLUID) {
-
-								finv = collideField[idx(xlength, x + dx, y + dy,
-										z + dz, Q - i - 1)];
-								collideField[idx(xlength, x, y, z, i)] = finv;
-							}
-						}
-					}/* Conditions for OUTFLOW */
-				}
+		for (int y = 0; y < xlength + 2; ++y) {
+			for (int z = 0; z < xlength + 2; ++z) {
+				computeNoSlipCondition(xlength, 0, y, z, i, dx, dy, dz, collideField, flagField);
+				computeNoSlipCondition(xlength, xlength+1, y, z, i, dx, dy, dz, collideField, flagField);
 			}
 		}
 	}
 }
-#else
-void treatBoundary(double *collideField, int* flagField,
-		const double * const wallVelocity, int xlength) {
-	int i, x, y, z, dx, dy, dz;
-	int N = xlength; /*substitution for the sake of simplicity*/
-	double *currentCell;
-	double finv, cu = 0;
-	double density;
-
-	/*Going through the hole boundary domain*/
-
-	/*For each boundary cell/particle we update all the distribution functions,
-	that point towards fluid, according to no-slip and moving-wall boundary conditions*/
-
-	/*Two opposite/parallel boundary planes*/
-	for (int k = 0; k < 2; ++k) {
-		x = k * (N + 1);
-		for (y = 0; y < N + 2; ++y) {
-			for (z = 0; z < N + 1; ++z) {
-				for (i = 0; i < Q; ++i) {
-					/*dx = c_i_x*dt, dt = 1*/
-					dx = LATTICEVELOCITIES[i][0];
-					dy = LATTICEVELOCITIES[i][1];
-					dz = LATTICEVELOCITIES[i][2];
-
-					/*Checking if the i-th directions is facing the fluid particles*/
-					/*y-z plane*/
-					if (x+dx > 0 && x+dx < N + 1 &&
-						y+dy > 0 && y+dy < N + 1 &&
-						z+dz > 0 && z+dz < N + 1) {
-
-						/*i-th distribution fun of boundary cell becomes distribution fun of
-						the inverse lattice velocity of the pointed particle/lattice */
-
-						/*Inverse lattice velocity to c_i is accessed
-						at Q-i-1-th position of (x+dx, y+dy, z+dz) cell*/
-
-						finv = collideField[Q * ((z+dz)*(N+2)*(N+2) + (y+dy)*(N+2) + x+dx) + Q-i-1];
-						collideField[Q * (z*(N+2)*(N+2) + y*(N+2) + x) + i] = finv;
-					}
-					/*x and y swapped, so that another pair of boundary planes may be accessed within the same iteration*/
-					/*x-z plane*/
-					if (y+dx > 0 && y+dx < N + 1 &&
-						x+dy > 0 && x+dy < N + 1 &&
-						z+dz > 0 && z+dz < N + 1) {
-
-						finv = collideField[Q * ((z+dz)*(N+2)*(N+2) + (x+dy)*(N+2) + y+dx) + Q-i-1];
-						collideField[Q * (z*(N+2)*(N+2) + x*(N+2) + y) + i] = finv;
-					}
-					/*x and z swapped, out of same reason*/
-					/*x-y plane*/
-					if (z+dx > 0 && z+dx < N + 1 &&
-						y+dy > 0 && y+dy < N + 1 &&
-						x+dz > 0 && x+dz < N + 1) {
-
-						finv = collideField[Q * ((x+dz)*(N+2)*(N+2) + (y+dy)*(N+2) + z+dx) + Q-i-1];
-
-						/*Additional term when we are considering moving-wall (top plane - k=1)*/
-						/*Velocity of the moving-wall is taken into account*/
-						if(k == 1) {
-							currentCell = collideField + Q * ((x+dz)*(N+2)*(N+2) + (y+dy)*(N+2) + z+dx);
-							computeDensity(currentCell, &density);
-							cu = 0; /*dot product of c_i and velocity of wall*/
-							for (int d = 0; d < D; ++d) {
-								cu += LATTICEVELOCITIES[i][d] * wallVelocity[d];
-							}
-							finv += 2*LATTICEWEIGHTS[i]*density*cu/(C_S*C_S);
-						}
-						collideField[Q * (x*(N+2)*(N+2) + y*(N+2) + z) + i] = finv;
-					}
-				}
-			}
-		}
-	}
-
-}
-#endif
