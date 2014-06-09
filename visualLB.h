f69b91ee5c0e09b03b4f779bb14f0cd3a47c3ede
Andre Malcher <andre.malcher@TUM>
2014-06-09 12:52:50 +0200

    producing pvts file for parallel output. not working yet though.

--- visualLB.h
+++ visualLB.h
@@ -1,6 +1,9 @@
 #ifndef _VISUALLB_H_
 #define _VISUALLB_H_
 
+void writeParallelVtkFile(const char * const filename, int iproc, int jproc, int kproc,
+        const int * const sublength, unsigned int t);
+
 /** writes the density and velocity field (derived from the distributions in collideField)
  *  to a file determined by 'filename' and timestep 't'. You can re-use parts of the code
  *  from visual.c (VTK output for Navier-Stokes solver) and modify it for 3D datasets.
